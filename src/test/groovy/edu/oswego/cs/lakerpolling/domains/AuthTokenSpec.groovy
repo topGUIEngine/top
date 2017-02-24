e4cc5dcdfdbb52d769dc24af920b30039df3f9f9
Jeff Registre <jeffreyregist365@gmail.com>
2017-02-24 02:21:02 -0500

    removed tests

--- src/test/groovy/edu/oswego/cs/lakerpolling/domains/AuthTokenSpec.groovy
+++ src/test/groovy/edu/oswego/cs/lakerpolling/domains/AuthTokenSpec.groovy
@@ -15,8 +15,4 @@ class AuthTokenSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
 }
--- src/test/groovy/edu/oswego/cs/lakerpolling/services/AuthServiceSpec.groovy
+++ src/test/groovy/edu/oswego/cs/lakerpolling/services/AuthServiceSpec.groovy
@@ -15,8 +15,5 @@ class AuthServiceSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
+
 }
