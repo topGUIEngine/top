70737cbf0af0aca8c5c8cc1cbe1c9e8383fc2e4d
zsabin <zsabin@gmail.com>
2017-03-05 16:49:26 -0500

    Added basic skeleton of CourseService class

--- grails-app/services/edu/oswego/cs/lakerpolling/services/CourseService.groovy
+++ grails-app/services/edu/oswego/cs/lakerpolling/services/CourseService.groovy
@@ -1,8 +1,46 @@
 package edu.oswego.cs.lakerpolling.services
 
+import edu.oswego.cs.lakerpolling.domains.Course
+import edu.oswego.cs.lakerpolling.domains.User
 import grails.transaction.Transactional
 
 @Transactional
 class CourseService {
 
+    Course make(User instructor, String courseName, String crn) {
+        Course course = new Course(name: courseName, CRN: crn)
+        //TODO: connect course with instructor
+        course.save(flush: true, failOnError: true)
+    }
+
+    void delete(int courseId) {
+        Course course = Course.get(courseId)
+        if (course != null) {
+            course.delete()
+        }
+    }
+
+    boolean isInstructorOf(User user, int courseId) {
+        Course course = Course.get(courseId)
+        if (course != null) {
+            //TODO: check if user is instructor of course
+        }
+
+        return false
+    }
+
+    void addStudent(int courseId, User student) {
+        Course course = Course.get(courseId)
+        if (course != null) {
+            //TODO: add student to course
+        }
+    }
+
+    boolean containsStudent(int courseId, User student) {
+        Course course = Course.get(courseId)
+        if (course != null) {
+            //TODO: check if course contains student
+        }
+        return false
+    }
 }
