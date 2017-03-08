7e15d566ddfa450cc963d7b6c014063d6050c004
jpost27 <jpost94@gmail.com>
2017-03-08 12:40:17 -0500

    Update BootStrap.groovy

--- grails-app/init/edu/oswego/cs/lakerpolling/controllers/BootStrap.groovy
+++ grails-app/init/edu/oswego/cs/lakerpolling/controllers/BootStrap.groovy
@@ -1,12 +1,11 @@
 package edu.oswego.cs.lakerpolling.controllers
 
-import edu.oswego.cs.lakerpolling.domains.AuthToken
+import edu.oswego.cs.lakerpolling.domains.Course
 import edu.oswego.cs.lakerpolling.domains.User
 
 class BootStrap {
 
     def init = { servletContext ->
-        def init = { servletContext ->
         // 5 Example Users
         new User(firstName: "Josh", lastName: "Post", email: "jpost@oswego.edu",
                 imageUrl: "http://cdn.business2community.com/wp-content/uploads/2016/03/Vd3MJo.jpg",
@@ -16,12 +15,12 @@ class BootStrap {
         new User(firstName: "Akeem", lastName: "Davis", email: "adavis20@oswego.edu",
                 imageUrl: null,role: RoleType.INSTRUCTOR,authToken: null).save(flush:true)
         new User(firstName: "Ricky", lastName: "Rojas", email: "rrojas@oswego.edu",
-                imageUrl: null, role: null,authToken: null,course: Course.findAllByName("csc480")).save(flush:true)
+                imageUrl: null, role: null,authToken: null,course: Course.findByCrn("csc480")).save(flush:true)
         new User(firstName: "Matt", lastName: "Wu", email: "jwu5@oswego.edu",
-                imageUrl: null,role: null,authToken: null,course: Course.findAllByName("csc480")).save(flush:true)
+                imageUrl: null,role: null,authToken: null,course: Course.findByCrn("csc480")).save(flush:true)
 
         //Example Course
-        new Course(name: csc480, crn: 12345, users: User.findAllByRole(null)).save(flush:true)
+        new Course(name: "csc480", crn: 12345, users: User.findAllByRole(null)).save(flush:true)
 
     }
     def destroy = {
