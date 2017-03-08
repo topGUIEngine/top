0c7f1eec933af318952db1e0109542934c07b59d
Jeff Registre <jeffreyregist365@gmail.com>
2017-03-08 15:11:57 -0500

    Updating code before writing endpoint.

--- grails-app/controllers/edu/oswego/cs/lakerpolling/controllers/CourseController.groovy
+++ grails-app/controllers/edu/oswego/cs/lakerpolling/controllers/CourseController.groovy
@@ -1,5 +1,7 @@
 package edu.oswego.cs.lakerpolling.controllers
 
+import edu.oswego.cs.lakerpolling.domains.AuthToken
+import edu.oswego.cs.lakerpolling.domains.User
 import edu.oswego.cs.lakerpolling.services.PrecheckService
 
 class CourseController {
@@ -11,7 +13,13 @@ class CourseController {
     def courseGet(String access_token, String user_id, String course_id, boolean list_students) {
         def require = precheckService.require(params, ["access_token", "user_id"])
         if (require.success) {
-            render("HI")
+            AuthToken token = AuthToken.findByAccessToken(access_token)
+            User reqUser = token?.user
+            if(token != null && reqUser != null) {
+
+            } else {
+                render(view : '../unauthorized')
+            }
         } else {
             render (view : '../failure', model : [errorCode:require.error, message:require.message])
         }
@@ -19,14 +27,17 @@ class CourseController {
 
     def putCourse(String access_token, String name, String crn, String user_id) {
         def require = precheckService.require(params, ["access_token", "name", "crn"])
+        render("crn:$crn")
     }
 
     def postCourse(String access_token, String course_id, String name, String user_id, File file) {
         def require = precheckService.require(params, ["access_token", "course_id"])
+        render "name:$name"
     }
 
     def deleteCourse(String access_token, String course_id, String user_id) {
         def require = precheckService.require(params, ["access_token", "course_id"])
+        render "course_id:$course_id"
     }
 
 }
--- grails-app/controllers/edu/oswego/cs/lakerpolling/controllers/UrlMappings.groovy
+++ grails-app/controllers/edu/oswego/cs/lakerpolling/controllers/UrlMappings.groovy
@@ -10,6 +10,9 @@ class UrlMappings {
 
         group "/api/course", {
             "/"(controller: 'course', action: 'courseGet', method : 'get')
+            "/"(controller: 'course', action: 'putCourse', method : 'put')
+            "/"(controller: 'course', action: 'postCourse', method : 'post')
+            "/"(controller: 'course', action: 'deleteCourse', method : 'delete')
         }
 
         "/**"(controller: 'application', action: 'index')
