53309e3b55dafba34ac6ada9b9e2bd988dfb3e03
zsabin <zsabin@gmail.com>
2017-03-10 15:32:48 -0500

    Added verifyInstructorAccess to CourseService

--- grails-app/services/edu/oswego/cs/lakerpolling/services/CourseService.groovy
+++ grails-app/services/edu/oswego/cs/lakerpolling/services/CourseService.groovy
@@ -3,54 +3,47 @@ package edu.oswego.cs.lakerpolling.services
 import edu.oswego.cs.lakerpolling.domains.Course
 import edu.oswego.cs.lakerpolling.domains.User
 import edu.oswego.cs.lakerpolling.util.QueryResult
+import edu.oswego.cs.lakerpolling.util.RoleType
+
 import grails.transaction.Transactional
+import org.springframework.http.HttpStatus
+
+import javax.management.Query
 
 @Transactional
 class CourseService {
+
+    class CourseErrors {
+        final static String UNAUTHORIZED_ACCESS = 'User is not authorized to perform action'
+    }
+
+    UserService userService
 
     QueryResult<Course> deleteCourse () {
 
     }
 
-//    Course make(User instructor, String courseName, String crn) {
-//        Course course = new Course(name: courseName, CRN: crn)
-//        //TODO: connect course with instructor
-//        course.save(flush: true, failOnError: true)
-//    }
-//
-//    void delete(int courseId) {
-//        Course course = Course.get(courseId)
-//        if (course != null) {
-//            course.delete()
-//        }
-//    }
-//
-//    boolean isInstructorOf(User user, int courseId) {
-//        Course course = Course.get(courseId)
-//        if (course != null) {
-//            //TODO: check if user is instructor of course
-//        }
-//
-//        return false
-//    }
-//
-//    boolean isInstructorOf(User user, Course course) {
-//        course.instructorId == user.id
-//    }
-//
-//    void addStudent(int courseId, User student) {
-//        Course course = Course.get(courseId)
-//        if (course != null) {
-//            //TODO: add student to course
-//        }
-//    }
-//
-//    boolean containsStudent(int courseId, User student) {
-//        Course course = Course.get(courseId)
-//        if (course != null) {
-//            //TODO: check if course contains student
-//        }
-//        return false
-//    }
+    QueryResult verifyInstructorAccess(User user, Course course) {
+        def result = new QueryResult()
+        if (user.role.type == RoleType.ADMIN || (user.role.type == RoleType.INSTRUCTOR && course.instructor.equals(user))) {
+            result.success = true
+            return result
+        }
+        result.success = false
+        result.message = CourseErrors.UNAUTHORIZED_ACCESS
+        result.errorCode = HttpStatus.UNAUTHORIZED.value()
+        result
+    }
 
+    QueryResult verifyInstructorAccess(String accessToken, Course course) {
+        def userResult = userService.getUser(accessToken)
+        if (!userResult.success) {
+            def result = new QueryResult()
+            result.success = false
+            result.errorCode = userResult.errorCode
+            result.message = userResult.message
+            return result
+        }
+        return verifyInstructorAccess(userResult.data, course)
+    }
 }
