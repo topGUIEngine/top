925ba9d8e8dcd7ec81231e06c1f9abc6b2b26e8c
Brandon Lanthrip <brandonlanthrip@Brandons-MacBook-Pro.local>
2017-04-08 15:00:40 -0400

    fixed errors in getanswers endpoint and a small error that happened when creating a question

--- grails-app/services/edu/oswego/cs/lakerpolling/services/QuestionService.groovy
+++ grails-app/services/edu/oswego/cs/lakerpolling/services/QuestionService.groovy
@@ -12,7 +12,11 @@ class QuestionService {
         List<String> tempList = answers.indexOf(",") != -1 ? answers.split(",").toList(): [answers]
         List<Boolean> answerList = new ArrayList<>()
 
-        tempList.forEach {s -> answerList.add(s.asBoolean())}
+        tempList.forEach {s ->
+            if(s == "false") answerList.add(false)
+            else if(s == "true") answerList.add(true)
+            else return null
+        }
         if (user) {
             if (user.role.type == RoleType.INSTRUCTOR) {
                 Course course = Course.findById(course_id.toLong())
@@ -25,7 +29,9 @@ class QuestionService {
                     }
                     newQuestion.active = false
                     newQuestion.studentAnswers = new ArrayList<>()
+                    println("Answer list size: " + answerList.size())
                     answerList.each { i -> newQuestion.studentAnswers.add(0) }
+                    println("new question student list size: " + newQuestion.studentAnswers.size())
                     newQuestion.save(flush: true, failOnError: true)
                     def attendance = Attendance.findByDateAndCourse(makeDate(date), course)
                     if (attendance == null) attendance = new Attendance(date: makeDate(date), course: course)
@@ -42,20 +48,33 @@ class QuestionService {
             if(user.role.getType() == RoleType.STUDENT) {
                 List<String> tempList = answer.indexOf(",") != -1 ? answer.split(",").toList(): [answer]
                 List<Boolean> answerList = new ArrayList<>()
-                tempList.forEach {s -> answerList.add(s.asBoolean())}
+                tempList.forEach {s ->
+                    if(s == "false") answerList.add(false)
+                    else if(s == "true") answerList.add(true)
+                    else return false
+                }
                 def question = Question.findById(question_id.toLong())
                 if(question) {
-                    def attendee = Attendance.findByDateAndCourse(makeDate(date), question.course).attendees.find { a -> a.student == user }
-                    if(attendee) {
-                        def isRight = false
-                        def realAnswers = question.answers
-                        realAnswers.eachWithIndex { a, i ->
-                            isRight = (a == answerList.get(i))
-                            if (answerList.get(i)) question.studentAnswers.add(i, question.studentAnswers.get(i) + 1)
-                        }
-                        attendee.attended = true
-                        new Answer(correct: isRight, question: question, student: user).save(flush: true, failOnError: true)
-                        attendee.attended
+                    if(question.active) {
+                        def attendee = Attendance.findByDateAndCourse(makeDate(date), question.course).attendees.find { a -> a.student == user }
+                        if (attendee) {
+                            def isRight = false
+                            def realAnswers = question.answers
+                            realAnswers.eachWithIndex { a, i ->
+                                isRight = (a == answerList.get(i))
+                                println("ANSWERLIST-STUDENT: " + answerList.get(i))
+                                if (answerList.get(i)) {
+                                    def num = question.studentAnswers.get(i)
+                                    num += 1
+                                    question.studentAnswers.remove(i)
+                                    question.studentAnswers.add(i, num)
+                                    println("ADDING 1 TO QUESTION")
+                                }
+                            }
+                            attendee.attended = true
+                            new Answer(correct: isRight, question: question, student: user).save(flush: true, failOnError: true)
+                            attendee.attended
+                        } else false
                     } else false
                 } else false
             } else false
