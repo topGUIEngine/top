5b82a52a206763f907d59e21ce8aed61e7ad64f3
zsabin <zsabin@gmail.com>
2017-04-09 17:25:48 -0400

    Fixed typo in UrlMappings

--- grails-app/controllers/edu/oswego/cs/lakerpolling/controllers/UrlMappings.groovy
+++ grails-app/controllers/edu/oswego/cs/lakerpolling/controllers/UrlMappings.groovy
@@ -36,7 +36,7 @@ class UrlMappings {
             "/"(controller: 'question', action: 'getQuestion', method: 'get')
             "/active"(controller: 'question', action: 'getActiveQuestion', method: 'get')
 
-            "/answer"(controller: 'question', action: 'getAnswers', mehtod: 'get')
+            "/answer"(controller: 'question', action: 'getAnswers', method: 'get')
             "/answer"(controller: 'question', action: 'answerQuestion', method: 'put')
         }
     }
